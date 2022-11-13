c9f85ecc735f06d2bf1efce12a2ff635d6158ca9
selipasha1337 <106588729+selipasha1337@users.noreply.github.com>
2022-11-13 05:59:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import Container from './components/Container'
 import MoviesTabs from './components/MoviesTabs'
 import { MoviesProvider } from './context/MoviesContext'
 
-// TODO: хранить id фильма и поставленную оценку в localStorage
-// TODO: оценка фильма и добавление в массив
-// TODO: фильтр, чтобы фильм нельзя было несколько раз оценить
-// TODO: вывод массива с карточками в таб Rated
-// TODO: возможность удалять оцененные фильмы
-
 class App extends Component {
   render() {
     return (
